cd5d2d10ec8c2f7180b079bffacda525f1303a40
Miguel Muscat <miguelmuscat93@gmail.com>
2017-11-17 16:47:42 +0100

    :white_check_mark: Added test for `PropelSqlExtractorInterface`

---
+++ test/unit/PropelSqlExtractorInterfaceTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace RebelCode\Migrations\UnitTest;
+
+use Xpmock\TestCase;
+use RebelCode\Migrations\PropelSqlExtractorInterface as TestSubject;
+
+/**
+ * Tests {@see TestSubject}.
+ *
+ * @since [*next-version*]
+ */
+class PropelSqlExtractorInterfaceTest extends TestCase
+{
+    /**
+     * The class name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'RebelCode\Migrations\PropelSqlExtractorInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return TestSubject
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+                     ->extract();
+
+        return $mock->new();
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(
+            static::TEST_SUBJECT_CLASSNAME,
+            $subject,
+            'A valid instance of the test subject could not be created.'
+        );
+    }
+}
